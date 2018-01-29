33b1f21388863de065e1c73de5c066d0a7f207df
dylandupreez <dylandp@privateproperty.co.za>
2018-01-29 09:13:05 +0200

    Ran compile and fixed merge conflicts that slipped through

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import ViewPropTypes from './viewPropTypes'
-import InteractionManager from "./interactionManager"
-
-export { InteractionManager, ViewPropTypes }
-=======
 import { ViewPropTypes } from "react-native";
 import InteractionManager from './interactionManager'
 
 export { InteractionManager, ViewPropTypes }
->>>>>>> ed4cc170b4b458b207825eadc9b8afa744b87130
