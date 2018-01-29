1ebb60e39b4a9c11bb5647205d6e4e11b80c90af
dylandupreez <dylandp@privateproperty.co.za>
2018-01-29 08:46:09 +0200

    Added: packages-lock.json to ignore

--- dist/src/basic/Tabs/Button.ios.js
+++ dist/src/basic/Tabs/Button.ios.js
@@ -1,4 +1,4 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.Button=undefined;var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/basic/Tabs/Button.ios.js";var _react=require("react");var _react2=_interopRequireDefault(_react);
+Object.defineProperty(exports,"__esModule",{value:true});exports.Button=undefined;var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src\\basic\\Tabs\\Button.ios.js";var _react=require("react");var _react2=_interopRequireDefault(_react);
 
 var _reactNative=require("react-native");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
 
